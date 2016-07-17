7cd8a053d7746b0f9b90a3acee975a7e94681b4f
Kien Ngoc Nguyen <tgsoon2002@gmail.com>
2016-07-17 16:46:59 -0700

    Project can add new object and they are appear in the list.

---
+++ Assets/Script/AssetDB.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AssetDB : MonoBehaviour
+{
+
+	public static AssetDB Instance;
+
+	//	public static AssetDB Instance {
+	//		get	{ return _instance; }
+	//	}
+
+	void Awake ()
+	{
+		if (Instance == null) {
+			DontDestroyOnLoad (gameObject);
+			Instance = this;
+		} else if (Instance != this) {
+			Destroy (gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
---
+++ Assets/Script/ObjectManager.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class  ObjectManager : MonoBehaviour
+{
+
+	private static ObjectManager _instance;
+	//[SerializeField]
+	private List<AssetBase> assetList;
+	private List<GameObject> objectNameList;
+	public GameObject assetPrefab;
+	public GameObject objectNamePre;
+	public Texture wallText;
+	public Texture floorText;
+
+	public static ObjectManager Instance {
+		get{ return  _instance; }
+	}
+
+
+
+	#region Unity Built-In
+
+	void Awake ()
+	{
+		_instance = this;
+		assetList = new List<AssetBase> ();
+		objectNameList = new List<GameObject> ();
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+	#endregion
+
+	public void RemoveItem (AssetBase assBeingRemove)
+	{
+		objectNameList.RemoveAll (o => o.name == assBeingRemove.randomID.ToString ());
+
+		assetList.Remove (assBeingRemove);
+	}
+
+	public void CreateAssets ()
+	{
+		Random rand = new Random ();
+		int id = -1;
+		do {
+			id = Random.Range (0, 1000);	
+		} while (objectNameList.Find (o => o.name == id.ToString ()));
+
+		GameObject tempAsset = Instantiate (assetPrefab, Vector3.zero, Quaternion.Euler (Vector3.zero)) as GameObject;
+		tempAsset.GetComponent<AssetBase> ().randomID = id;
+		GameObject tempAssetName = Instantiate (objectNamePre) as GameObject;
+		tempAssetName.transform.parent = transform;
+		tempAssetName.name = id.ToString ();
+		assetList.Add ((AssetBase)tempAsset.GetComponent<AssetBase> ());
+		objectNameList.Add (tempAssetName);
+	}
+
+	public void CreateAssets (Vector3 position)
+	{
+
+
+	}
+
+
+
+}
