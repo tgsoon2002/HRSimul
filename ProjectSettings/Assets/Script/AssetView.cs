5aca876d846b7144b397531a7f564bd1fe41e419
VRCampbell <vrcampbell@csu.fullerton.edu>
2016-08-03 17:36:44 -0700

    you can change the color of the wall

---
+++ ProjectSettings/Assets/Script/AssetView.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AssetView : MonoBehaviour
+{
+	public AssetType assetType;
+	public Texture aTexture;
+	public Text countText;
+	public int aCount = 0;
+	//Destroy(ObjectManager.Instance.assetList.Find(0->0.randomID == value))
+
+	public int UsedCount {
+		get{ return  aCount; }
+		set {
+			aCount = value; 
+			countText.text = aCount.ToString ();
+		}
+	}
+
+	public void _CreateObject ()
+	{
+		if (ObjectManager.Instance != null) {
+			if (assetType != AssetType.Texture) {
+				ObjectManager.Instance.CreateAssets (name, assetType);
+			} else {
+
+			}
+			AssetDB.Instance._CloseViewer ();
+		}
+	}
+
+
+}
